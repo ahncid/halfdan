ea32b232ae6e1aea6037da65e814d86b0a0e5127
ahncid <ahncid@gmail.com>
2023-10-13 19:03:15 +0200

    fixed jitters

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 const headerTextElements = document.querySelectorAll(
-  "header .header-text, header .header-text2, header .header-text3, header .text4, header h1"
+  "header .header-text, header .header-text2, header .header-text3, header .text4"
 );
 const chevron = document.querySelector(".fa-chevron-down");
 const content = document.querySelector(".content");
@@ -13,16 +13,16 @@ window.addEventListener("scroll", function () {
   // Calculate the transform for the text based on scroll position
   const translateYValueText = window.scrollY / 2.5;
 
-  // Apply the transform to each text element inside the header
+  // Apply the transform to each text element inside the header using translate3d
   headerTextElements.forEach((textElement) => {
-    textElement.style.transform = `translateY(-${translateYValueText}px)`;
+    textElement.style.transform = `translate3d(0, -${translateYValueText}px, 0)`;
   });
 
-  // Apply the transform to the chevron to move at the same speed as the content
-  chevron.style.transform = `translateY(${translateYValueContent}px)`;
+  // Apply the transform to the chevron using translate3d
+  chevron.style.transform = `translate3d(0, ${translateYValueContent}px, 0)`;
 
-  // Apply the transform to the content container
-  content.style.transform = `translateY(${translateYValueContent}px)`;
+  // Apply the transform to the content container using translate3d
+  content.style.transform = `translate3d(0, ${translateYValueContent}px, 0)`;
 
   // Adjust the z-index based on scroll position
   if (window.scrollY > 50) {
@@ -60,6 +60,7 @@ iframeDivs.forEach((div, index) => {
   addHoverListeners(div, iframes[index]);
   addHoverListeners(iframes[index], div);
 });
+
 const spotifyDiv = document.querySelector(".spotify");
 const spotifyIframe = document.querySelector(".iframe-spotify");
 
