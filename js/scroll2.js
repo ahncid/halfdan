4386c2c29c0f80cdadd6bb075e92ff2b5f35c5e0
ahncid <ahncid@gmail.com>
2023-10-14 11:41:17 +0200

    no changes rly

---
+++ js/scroll2.js
@@ -0,0 +1,10 @@
+/* const container = document.querySelector(".another-container");
+
+container.addEventListener("scroll", function () {
+  const currentScrollPosition = container.scrollTop;
+
+  if (currentScrollPosition < scrollPoint) {
+    container.scrollTo(0, scrollPoint);
+  }
+});
+*/
